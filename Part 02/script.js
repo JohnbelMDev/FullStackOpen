ada72418bfd4b58eee6862b9923f466bd8704c7e
johnbelmdev <johnbel458@gmail.com>
2020-11-02 07:12:23 -0500

    commit

---
+++ Part 02/script.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App notes={notes} />,
+  document.getElementById('root')
+)
